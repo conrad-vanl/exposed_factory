b1fdbcdca8501d789d35fb0169a07a913b002898
Conrad VanLandingham <conrad@landdolph.in>
2013-11-15 10:31:34 -0600

    inject :include_in_exposed_factory helper into ActiveRecord

--- lib/exposed_factory.rb
+++ lib/exposed_factory.rb
@@ -1,1 +1,10 @@
 require "rack/exposed_factory"
+
+if !!Rails
+  ActiveRecord::Base.send(:define_method, :include_in_exposed_factory) do 
+    @include_in_exposed_factory
+  end
+  ActiveRecord::Base.send(:define_method, :include_in_exposed_factory=) do |arg| 
+    @include_in_exposed_factory = arg
+  end
+end
